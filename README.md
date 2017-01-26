0c6192fa82922b38e351a1f1c4a6fcd0416c7eef
Joel Purra <code+github@joelpurra.com>
2017-01-26 21:42:28 +0100

    Slight cleanup of the gulpfile, add development instructions to readme

--- README.md
+++ README.md
@@ -5,6 +5,30 @@ Played around with the canvas for use in the background of a website, and ended 
 - https://en.wiktionary.org/wiki/hexagon
 - https://en.wiktionary.org/wiki/gonif
 
+
+
+## Development
+
+Requires [`node`](https://nodejs.org) with `npm` installed.
+
+```shell
+# Get dependencies.
+npm install
+
+# Puts files in /docs/ for static viewing.
+npm run --silent build
+
+# Starts a development server and monitors files for changes.
+npm run --silent start
+
+# Go to the default development server address.
+open "http://localhost:47333/"
+```
+
+
+
 ---
+
+
 
 Copyright (c) 2014, 2015 [hexagonif](https://github.com/joelpurra/hexagonif) by [Joel Purra](https://joelpurra.com/). Licensed under the [GNU Affero General Public License, Version 3 (AGPL-3.0)](https://www.gnu.org/licenses/agpl-3.0.html).
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 
 /*global require: true, console: true, __dirname: true */
+/* eslint-disable no-console */
 
 var start = new Date().valueOf(),
 
-    gulp = require('gulp'),
-    path = require('path'),
-    size = require('gulp-size'),
-    browserify = require('gulp-browserify'),
+    gulp = require("gulp"),
+    path = require("path"),
+    size = require("gulp-size"),
+    browserify = require("gulp-browserify"),
+
+    rimraf = require("rimraf"),
 
     paths = {
         input: {
-            main: 'src/resources/javascript/main.js',
-            html: 'src/*.html',
-            javascript: 'src/resources/javascript/**/*.js',
-            libraries: 'src/resources/javascript/libraries/**/*.js',
-            json: 'src/resources/json/*.json',
-            css: 'src/resources/css/*.css'
+            main: "src/resources/javascript/main.js",
+            html: "src/*.html",
+            javascript: "src/resources/javascript/**/*.js",
+            libraries: "src/resources/javascript/libraries/**/*.js",
+            json: "src/resources/json/*.json",
+            css: "src/resources/css/*.css",
         },
         output: {
-            clean: 'dist/**',
-            html: 'dist/public/',
-            javascript: 'dist/public/resources/javascript/',
-            libraries: 'dist/public/resources/javascript/libraries/',
-            json: 'dist/public/resources/json/',
-            css: 'dist/public/resources/css/'
-        }
+            clean: "dist/",
+            html: "dist/public/",
+            javascript: "dist/public/resources/javascript/",
+            libraries: "dist/public/resources/javascript/libraries/",
+            json: "dist/public/resources/json/",
+            css: "dist/public/resources/css/",
+        },
     },
 
     refresh,
@@ -33,93 +36,102 @@ var start = new Date().valueOf(),
     options = {
         express: {
             root: path.resolve(__dirname, paths.output.html),
-            port: 47333
+            port: 47333,
         },
         size: {
-            showFiles: true
-        }
+            showFiles: true,
+        },
+    },
+
+    getServerUrl = function() {
+        return "http://localhost:" + options.express.port + "/";
     },
 
     stop = new Date().valueOf();
 
-console.log('Loading took ' + (stop - start) + ' ms.');
+console.log("Loading took " + (stop - start) + " ms.");
 
-gulp.task('clean', function() {
-    var clean = require('gulp-clean');
+gulp.task("clean", function(done) {
+    var rimrafOptions = {
+        maxBusyTries: 10,
+    };
 
-    return gulp.src(paths.output.clean, {
-        read: false
-    })
-        .pipe(clean());
+    function rmrf(next) {
+        rimraf(paths.output.clean, rimrafOptions, next);
+    }
+
+    // Try to delete twice due to bugs.
+    rmrf(rmrf.bind(null, done));
 });
 
-gulp.task('html', function() {
-    gulp.src(paths.input.html)
+gulp.task("html", function() {
+    return gulp.src(paths.input.html)
         .pipe(size(options.size))
         .pipe(gulp.dest(paths.output.html));
 });
 
-gulp.task('javascript-debug', function() {
-    gulp.src(paths.input.main)
+gulp.task("javascript-debug", function() {
+    return gulp.src(paths.input.main)
         .pipe(size(options.size))
         .pipe(browserify({
-            debug: true
+            debug: true,
         }))
         .pipe(size(options.size))
         .pipe(gulp.dest(paths.output.javascript));
 });
 
-gulp.task('javascript-production', function() {
-    var stripDebug = require('gulp-strip-debug'),
-        uglify = require('gulp-uglify'),
-        rename = require('gulp-rename');
+gulp.task("javascript-production", function() {
+    var stripDebug = require("gulp-strip-debug"),
+        uglify = require("gulp-uglify"),
+        rename = require("gulp-rename");
 
-    gulp.src(paths.input.main)
+    return gulp.src(paths.input.main)
         .pipe(size(options.size))
         .pipe(stripDebug())
         .pipe(size(options.size))
         .pipe(browserify())
         .pipe(size(options.size))
         .pipe(uglify())
-        .pipe(rename('main.min.js'))
+        .pipe(rename("main.min.js"))
         .pipe(size(options.size))
         .pipe(gulp.dest(paths.output.javascript));
 });
 
-gulp.task('javascript-libraries', function() {
-    gulp.src(paths.input.libraries)
+gulp.task("javascript-libraries", function() {
+    return gulp.src(paths.input.libraries)
         .pipe(size(options.size))
         .pipe(gulp.dest(paths.output.libraries));
 });
 
-gulp.task('javascript', ['javascript-debug', 'javascript-production', 'javascript-libraries']);
+gulp.task("javascript", ["javascript-debug", "javascript-production", "javascript-libraries"]);
 
-gulp.task('css', function() {
-    var styl = require('gulp-styl');
+gulp.task("css", function() {
+    var styl = require("gulp-styl");
 
-    gulp.src(paths.input.css)
+    return gulp.src(paths.input.css)
         .pipe(size(options.size))
         .pipe(styl())
         .pipe(size(options.size))
         .pipe(gulp.dest(paths.output.css));
 });
 
-gulp.task('json', function() {
-    var stripJsonComments = require('gulp-strip-json-comments');
+gulp.task("json", function() {
+    var stripJsonComments = require("gulp-strip-json-comments");
 
-    gulp.src(paths.input.json)
+    return gulp.src(paths.input.json)
         .pipe(size(options.size))
         .pipe(stripJsonComments())
         .pipe(size(options.size))
         .pipe(gulp.dest(paths.output.json));
 });
 
-gulp.task('livereload-server', function() {
-    var Q = require('q'),
-        deferred = Q.defer(),
-        livereloadServer = require('tiny-lr')(),
-        livereload = require('gulp-livereload');
+gulp.task("build", ["html", "javascript", "json", "css"]);
 
+gulp.task("livereload-server", function() {
+    var Q = require("q"),
+        deferred = Q.defer(),
+        livereloadServer = require("tiny-lr")(),
+        livereload = require("gulp-livereload");
 
     refresh = function() {
         console.error("Refresh perfomed before livereload-server was finished.");
@@ -140,12 +152,13 @@ gulp.task('livereload-server', function() {
     return deferred.promise;
 });
 
-gulp.task('server', ['livereload-server'], function() {
-    var Q = require('q'),
+gulp.task("server", ["livereload-server"], function() {
+    var Q = require("q"),
         deferred = Q.defer(),
-        express = require('express'),
-        livereloadClient = require('connect-livereload')(),
-        app = express();
+        express = require("express"),
+        livereloadClient = require("connect-livereload")(),
+        app = express(),
+        url = getServerUrl();
 
     app.use(livereloadClient);
     app.use(express.static(options.express.root));
@@ -154,6 +167,8 @@ gulp.task('server', ['livereload-server'], function() {
         if (error) {
             deferred.reject(error);
         } else {
+            console.log("Server started: " + url);
+
             deferred.resolve();
         }
     });
@@ -161,32 +176,32 @@ gulp.task('server', ['livereload-server'], function() {
     return deferred.promise;
 });
 
-gulp.task('open', ['server'], function() {
-    var open = require('gulp-open'),
-        url = 'http://localhost:' + options.express.port + '/';
+gulp.task("open", ["server"], function() {
+    var open = require("gulp-open"),
+        url = getServerUrl();
 
     // Any file that exists will do
-    return gulp.src('gulpfile.js')
-        .pipe(open('', {
-            url: url
+    return gulp.src("gulpfile.js")
+        .pipe(open("", {
+            url: url,
         }));
 });
 
-gulp.task('watch', ['livereload-server'], function() {
-    var tasks = ['html', 'javascript', 'json', 'css'],
+gulp.task("watch", ["build", "livereload-server"], function() {
+    var tasks = ["html", "javascript", "json", "css"],
         changeLogger = function(event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running task...');
+            console.log("File " + event.path + " was " + event.type + ", running task...");
         },
         livereloader = function(event) {
             gulp.src(event.path)
                 .pipe(refresh());
         };
 
-    tasks.forEach(function(task) {
-        gulp.watch(paths.input[task], [task])
-            .on('change', changeLogger)
-            .on('change', livereloader);
+    return tasks.map(function(task) {
+        return gulp.watch(paths.input[task], [task])
+            .on("change", changeLogger)
+            .on("change", livereloader);
     });
 });
 
-gulp.task('default', ['clean', 'server', 'html', 'javascript', 'json', 'css', 'watch']);
+gulp.task("default", ["clean", "server", "build", "watch"]);
